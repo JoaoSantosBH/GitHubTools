cf4246debf5271d2aae51e367f84a8281b26b966
João Santos <joaomarcelo.ms@gmail.com>
2023-08-30 12:20:47 -0300

    gradle des e versions

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+
+    //Core Section
+    const val androidCoreXVersion = "1.10.1"
+    const val androidxLifeCycleVersion = "2.6.1"
+    const val androidxActivityVersion = "1.7.2"
+
+    // Tests Section
+    const val junitVersion = "4.13.2"
+    const val junitTestVersion = "1.1.5"
+    const val espressoVersion = "3.5.1"
+
+}
