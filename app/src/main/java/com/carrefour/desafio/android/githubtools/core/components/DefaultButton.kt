26395c51aba207c28ce4b9861b7cb6bf9776345c
João Santos <joaomarcelo.ms@gmail.com>
2023-09-01 08:40:09 -0300

    adicionando UI compose Home

---
+++ app/src/main/java/com/carrefour/desafio/android/githubtools/core/components/DefaultButton.kt
@@ -0,0 +1,47 @@
+package com.carrefour.desafio.android.githubtools.core.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.carrefour.desafio.android.githubtools.features.home.presentation.HomeUiStates
+
+
+@Composable
+fun DefaultAppButton(state: HomeUiStates, btnText: Int, testTag: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        Button(
+            onClick = { },
+            enabled = state.isLoading,
+            shape = RoundedCornerShape(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .testTag(testTag)
+                .height(50.dp)
+        ) {
+            Text(
+                text = stringResource(id = btnText),
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
